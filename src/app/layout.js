f739ffcad29ac650d42223ff02e8ab93f39d6024
Saddam <76914007+Saddam550@users.noreply.github.com>
2025-05-16 20:16:51 +0600

    layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,6 +18,8 @@ export const metadata = {
   keywords:"Front-End Developer, WordPress, Bangladesh, React JS frameworks, Front-End, Developer, and WordPress, customizing, expert From Bangladesh, I am also, proficient in working, with React JS frameworks, creating websites, and designing them is my passion, saddam, hossen, saddan hossen,",
   robots:"noindex, nofollow",
   author:"Md Saddam Hossen",
+  name:"google-site-verification",
+  content:"CfH8nw_b0Com6bg2nOj3tBCkCRu0fcXjV8h0N2qpcUY",
   icons:{
     icon:[ "/favicon.ico?v=4"],
     apple:["/apple-touch-icon.png"],
